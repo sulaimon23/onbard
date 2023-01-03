4ef303f2553a6271af4a4eb8d75fd56a4d3c7796
Taiwo Sulaimon <tsulaimon96@gmail.com>
2023-01-03 15:11:36 +0100

    style: Theme configuration

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NativeBaseProvider, Text, Box, extendTheme } from "native-base";
+import theme from "./helpers/themeConfig";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    //
+    const customTheme = extendTheme({
+        ...theme,
+    });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    //
+    return (
+        <NativeBaseProvider theme={customTheme}>
+            <Box
+                flex={1}
+                bg="primary.900"
+                alignItems="center"
+                justifyContent="center"
+            >
+                <Text>Open up App.js to start working on your app!</Text>
+            </Box>
+        </NativeBaseProvider>
+    );
+}
---
+++ helpers/themeConfig.js
@@ -0,0 +1,23 @@
+export default theme = {
+    colors: {
+        // Add new color
+        primary: {
+            50: "#E3F2F9",
+            100: "#C5E4F3",
+            200: "#A2D4EC",
+            300: "#7AC1E4",
+            400: "#47A9DA",
+            500: "#0088CC",
+            600: "#007AB8",
+            700: "#006BA1",
+            800: "#005885",
+            900: "#fff000",
+        },
+        // Redefining only one shade, rest of the color will remain same.
+        amber: {
+            400: "#d97706",
+        },
+    },
+    fontSizes: {},
+    fonts: {},
+};
